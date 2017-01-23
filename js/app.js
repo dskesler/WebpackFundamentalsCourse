2c7105b99ea6a3fb0aa68ff38ff0cac6c5b06e34
dskesler <skesler@ebsco.com>
2017-01-23 15:17:46 -0600

    chapter 4 css modules

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ console.log('App loaded');
 //Order is important! want app.css to override bootstrap.css
 require('../css/bootstrap.css');
 require('../css/app.css');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,29 @@
 var path = require('path');
 
 module.exports = {
-    context: path.resolve('js'),
-    entry: ["./app"],
-    output: {
-        path: path.resolve("build/js/"),
-        publicPath: "/public/assets/js/",
-        filename: "bundle.js" //name === entry.key
-    },
+	context: path.resolve('js'),
+	entry: ["./app"],
+	output: {
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
+		filename: "bundle.js"
+	},
 
-    devServer: {
-        contentBase: 'public'
-    },
+	devServer: {
+		contentBase: 'public'
+	},
 
-    module: {
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader"
+			}
+		]
+	},
 
-        /*
-        preloaders: [
-                {
-                    test: /\.js$/,
-                    exclude: 'node_modules',
-                    loader: 'jshint-loader'
-                }
-        ],
-        */
-        loaders: [
-           /* { 
-                test: /\.es6$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            */
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                loader: "style-loader!css-loader"
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: ['', '.js', '.es6']
-    }
+	resolve: {
+		extensions: ['', '.js', '.es6']
+	}
 }
